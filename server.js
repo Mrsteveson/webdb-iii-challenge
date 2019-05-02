a8d70a508b4647e6f1601948fdc73a6616ca56c0
mrsteveson <patrick.steveson727@gmail.com>
2019-05-01 19:07:37 -0500

    stretch getById working properly.

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const helmet = require('helmet');
 const server = express();
 // Import routers
 const cohortsRouter = require('./cohorts/cohorts-router.js');
+const studentsRouter = require('./students/students-router.js');
 
 server.use(helmet());
 server.use(express.json());
+
 // Use Router
-server.use('/api/cohorts', cohortsRouter)
+server.use('/api/cohorts', cohortsRouter);
+server.use('/api/students', studentsRouter);
 
 // Hello msg
 server.get('/', (req, res) => {
